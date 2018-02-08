515b23172f02b815ecef4b5fffc2211ba78b19b3
hewang <hewang2@cisco.com>
2018-02-08 10:35:21 +0800

    test

---
+++ JavaTrian/LearnJava002DefaultInitialization.java
@@ -0,0 +1,13 @@
+public class LearnJava002DefaultInitialization {
+    int i;
+    char c;
+
+    public LearnJava002DefaultInitialization() {//构造函数，这是一个方法
+        System.out.println("i=" + i);
+        System.out.println("c=[" + c + "]");
+    }
+
+    public static void main(String[] args) {
+        new LearnJava002DefaultInitialization();
+    }
+}
