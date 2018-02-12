2a35ae8f5ea95209001ae3898eca40c5206093a1
wanghe <yueyesuifeng@sina.com>
2018-02-12 22:41:41 +0800

    Java

--- JavaTrain/LearnJava002DefaultInitialization.java
+++ JavaTrain/LearnJava002DefaultInitialization.java
@@ -1,12 +1,26 @@
+/**
+ * 基本数据类型的默认值
+ * boolean char byte short int long float double
+ */
 public class LearnJava002DefaultInitialization {
-    int i;
+    boolean flag;
     char c;
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
 
     public LearnJava002DefaultInitialization() {//构造函数，这是一个方法
+        System.out.println("flage="+flag);
+        System.out.println("c=" + c+ "");          
+        System.out.println("b="+b);
+        System.out.println("s="+s);
         System.out.println("i=" + i);
-        System.out.println("c=[" + c + "]");  
-        System.out.println("dev");
-         System.out.println("test");
+        System.out.println("l="+l);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
     }
 
     public static void main(String[] args) {
