dca7d484108f482bec9b3138057bdacca9189fd7
wanghe <yueyesuifeng@sina.com>
2018-02-12 23:12:27 +0800

    static的作用

--- JavaTrain/LearnJava004Incrementable.java
+++ JavaTrain/LearnJava004Incrementable.java
@@ -1,15 +1,17 @@
 /**
  * 当声明一个事物是static时，就意味着这个域或方法不会与包含它的那个类的任何对象实例关联在一起
  */
-class StaticTest{
+class StaticTest {
     static int i = 47;
 }
-public class LearnJava004Incrementable{
-    static void increment(){
+
+public class LearnJava004Incrementable {
+    static void increment() {
         StaticTest.i++;
     }
-    public static void main(String[] args){
-        LearnJava004Incrementable sf =new LearnJava004Incrementable();
+
+    public static void main(String[] args) {
+        LearnJava004Incrementable sf = new LearnJava004Incrementable();
         sf.increment();
         LearnJava004Incrementable.increment();
         increment();
