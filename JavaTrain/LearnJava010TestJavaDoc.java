e6627b10adf3e316675aa93b4e482eb4eb24c4c7
wanghe <yueyesuifeng@sina.com>
2018-02-13 18:10:10 +0800

    测试生成api文档

---
+++ JavaTrain/LearnJava010TestJavaDoc.java
@@ -0,0 +1,26 @@
+public class LearnJava010TestJavaDoc {
+    /**
+     * Description:
+     * <br> 这是一个测试的类
+     * <br> 创建时间 2018年2月13号
+     * @author hewang
+     * @version 1.0
+    */
+
+    /**
+     * 对象成员编码
+     */
+    private int id;
+    /**
+     * 对象成员名字
+    */
+    private String name;
+    /**
+     * 这是里面的一个方法
+     * @param name 这是一个方法参数
+     * @return 这是返回值
+    */
+    public void sayHello(String name) {
+
+    }
+}
