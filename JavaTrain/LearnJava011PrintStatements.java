736efc1f8630663e6000afa16757489195827be2
wanghe <yueyesuifeng@sina.com>
2018-02-14 20:03:42 +0800

    PrintStatement

---
+++ JavaTrain/LearnJava011PrintStatements.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class LearnJava011PrintStatements{
+    public static void main(String[] args) {
+        Date currDate = new Date();
+        System.out.println("Hello,it's:" + currDate);
+    }
+}
