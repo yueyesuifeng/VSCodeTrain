937ffa62186335ee7acc402c8c97ac61c61bfe7f
wanghe <yueyesuifeng@sina.com>
2018-02-16 21:20:21 +0800

    字符的二进制形式

---
+++ JavaTrain/LearnJava023BinaryChar.java
@@ -0,0 +1,11 @@
+/**
+ * 字符的二进制形式
+ */
+public class LearnJava023BinaryChar{
+    public static void main(String[] args) {
+        System.out.println("A: " + Integer.toBinaryString('A'));
+        System.out.println("!: " + Integer.toBinaryString('!'));
+        System.out.println("x: " + Integer.toBinaryString('x'));
+        System.out.println("7: " + Integer.toBinaryString('7'));
+    }
+}
