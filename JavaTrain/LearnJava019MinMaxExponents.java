b15f1f1c42090275655fb660e898c23a314c8f7b
wanghe <yueyesuifeng@sina.com>
2018-02-16 20:18:09 +0800

    指数形式

---
+++ JavaTrain/LearnJava019MinMaxExponents.java
@@ -0,0 +1,11 @@
+/**
+ * 用指数的方式显示float和double的最大和最小值
+*/
+public class LearnJava019MinMaxExponents {
+    public static void main(String[] args) {
+        System.out.println("Float MIN:" + Float.MIN_VALUE);
+        System.out.println("Float MAX:" + Float.MAX_VALUE);
+        System.out.println("Double MIN:" + Double.MIN_VALUE);
+        System.out.println("Double MAX:" + Double.MAX_VALUE);
+    }
+}
