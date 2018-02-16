a7b33f320cc01e44c40997b862bda7a553a4bdc3
wanghe <yueyesuifeng@sina.com>
2018-02-16 21:07:06 +0800

    无符号右移操作

---
+++ JavaTrain/LearnJava022UnsignedRightShift.java
@@ -0,0 +1,41 @@
+/**
+ * 无符号右移操作
+*/
+public class LearnJava022UnsignedRightShift {
+    public static void main(String[] args) {
+      int i = -1 << 1;
+      System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      System.out.println(i);
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+      i >>>= 1;  System.out.println(Integer.toBinaryString(i));
+    }
+  }
