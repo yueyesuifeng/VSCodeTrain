94c7c68fa9ec4d5885363651cddf9360182fd05c
wanghe <yueyesuifeng@sina.com>
2018-02-17 19:44:34 +0800

    范围测试

---
+++ JavaTrain/LearnJava030To98.java
@@ -0,0 +1,11 @@
+public class LearnJava030To98 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 100; i++) {
+			if (i == 98) {
+				break;//break跳出整个for循环
+			}
+			System.out.print(i + " ");
+		}
+	}
+
+}
