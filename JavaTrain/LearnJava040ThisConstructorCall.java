ce130a47e23383fc1a09a4f9f82d951183b80726
wanghe <yueyesuifeng@sina.com>
2018-02-19 21:20:50 +0800

    构造器中调用构造器

--- JavaTrain/LearnJava040ThisConstructorCall.java
+++ JavaTrain/LearnJava040ThisConstructorCall.java
@@ -1,5 +1,6 @@
 /**
- * 在构造器中调用另一个构造器
+ * 在构造器中调用另一个构造器，尽管可以用this调用一个构造器，但却不能调用两个
+ * 必须将构造器调用放在最起始处，否则编译器会报错
 */
 public class LearnJava040ThisConstructorCall {
     public LearnJava040ThisConstructorCall(String s) {
@@ -7,10 +8,12 @@ public class LearnJava040ThisConstructorCall {
     }
 
     public LearnJava040ThisConstructorCall(int i) {
+        // System.out.println("test");//必须将构造器调用放在最起始处，否则编译器会报错
         this("i=" + i);
     }
 
     public static void main(String[] args) {
+        System.out.println("test");
         new LearnJava040ThisConstructorCall("String call");
         new LearnJava040ThisConstructorCall(47);
     }
