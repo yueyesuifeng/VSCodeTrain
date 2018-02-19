53d58edc51a7376ffc0d119995d821e9255202e6
wanghe <yueyesuifeng@sina.com>
2018-02-19 20:17:27 +0800

    默认的构造函数

---
+++ JavaTrain/LearnJava035DefaultConstructor.java
@@ -0,0 +1,8 @@
+public class LearnJava035DefaultConstructor{
+    LearnJava035DefaultConstructor(){
+        System.out.println("Default Constructor");
+    }
+    public static void main(String[] args) {
+        new LearnJava035DefaultConstructor();
+    }
+}
